9191d02a94d981fa8b8f0d5f55f99b89a66f5336
Brecht Ooms <brecht.ooms@hotmail.com>
2020-08-29 22:19:30 +0200

    Add semicolons and change indents to tabs
    Change line endings back to unix style LF

--- db.js
+++ db.js
@@ -1,27 +1,27 @@
-var mysql = require("mysql")
+var mysql = require("mysql");
 
 var mysql_pool = mysql.createPool({
-  connectionLimit: 100,
-  host: "localhost",
-  user: "elements",
-  password: "sdfiasukas4d65a4sd64a6s4d64a6sda4sd6a45s6d4a6s546d9FG496SDF4G64SFDG78ADFGADJFKSADHFJLSADFVA",
-  database: "elements"
-})
+	connectionLimit: 100,
+	host: "localhost",
+	user: "elements",
+	password: "sdfiasukas4d65a4sd64a6s4d64a6sda4sd6a45s6d4a6s546d9FG496SDF4G64SFDG78ADFGADJFKSADHFJLSADFVA",
+	database: "elements"
+});
 
-function GetConnection (toDo) {
-  mysql_pool.getConnection(function (err, connection) {
-    if (err) {
-      console.log(" Error getting mysql_pool connection: " + err)
-      throw err
-    }
-    toDo(connection)
-    connection.release()
-  })
+function GetConnection(toDo) {
+	mysql_pool.getConnection(function (err, connection) {
+		if (err) {
+			console.log(" Error getting mysql_pool connection: " + err);
+			throw err;
+		}
+		toDo(connection);
+		connection.release();
+	});
 }
-function escape (value) {
-  return mysql.escape(value)
+function escape(value) {
+	return mysql.escape(value);
 }
 module.exports = {
-  GetConnection,
-  escape
-}
+	GetConnection,
+	escape
+};
--- logger.js
+++ logger.js
@@ -1,38 +1,39 @@
-require("colors")
-var fs = require("fs")
+require("colors");
+var fs = require("fs");
 
-var log = fs.createWriteStream(__dirname + "/server.log")
+// eslint-disable-next-line no-undef
+var log = fs.createWriteStream(__dirname + "/server.log");
 
-function info (data, req = null) {
-  if (req) {
-    data += "   by " + (req.steamID ? req.steamID : req.connection.remoteAddress)
-  }
-  const time = new Date()
-  log.write("[" + time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds() + "] (INFO) " + data + "\n")
-  console.log(data.cyan.bold)
+function info(data, req = null) {
+	if (req) {
+		data += "   by " + (req.steamID ? req.steamID : req.connection.remoteAddress);
+	}
+	const time = new Date();
+	log.write("[" + time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds() + "] (INFO) " + data + "\n");
+	console.log(data.cyan.bold);
 }
-function reqInfo (req) {
-  info(req.method + " " + req.originalUrl, req)
+function reqInfo(req) {
+	info(req.method + " " + req.originalUrl, req);
 }
-function error (data, req = null) {
-  if (req != null) {
-    data += "   by " + (req.steamID ? req.steamID : req.connection.remoteAddress)
-  }
-  const time = new Date()
-  log.write("[" + time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds() + "] (ERROR) " + data + "\n")
-  console.log(data.red.bold)
+function error(data, req = null) {
+	if (req != null) {
+		data += "   by " + (req.steamID ? req.steamID : req.connection.remoteAddress);
+	}
+	const time = new Date();
+	log.write("[" + time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds() + "] (ERROR) " + data + "\n");
+	console.log(data.red.bold);
 }
-function success (data, req = null) {
-  if (req) {
-    data += "   by " + (req.steamID ? req.steamID : req.connection.remoteAddress)
-  }
-  const time = new Date()
-  log.write("[" + time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds() + "]  (SUCCESS) " + data + "\n")
-  console.log(data.green.bold)
+function success(data, req = null) {
+	if (req) {
+		data += "   by " + (req.steamID ? req.steamID : req.connection.remoteAddress);
+	}
+	const time = new Date();
+	log.write("[" + time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds() + "]  (SUCCESS) " + data + "\n");
+	console.log(data.green.bold);
 }
 module.exports = {
-  info,
-  error,
-  success,
-  reqInfo
-}
+	info,
+	error,
+	success,
+	reqInfo
+};
